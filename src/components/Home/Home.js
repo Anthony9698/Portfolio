045abbe75d42b1ba79ac83f3b6990f59e629df9b
Anthony9698 <anthonyviera4@gmail.com>
2021-02-24 13:52:01 -0600

    Added functional social media links to home page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import profilePic from '../../assets/Home/Profile/me.png';
+import gitHubLink from '../../assets/Home/Social/github.png';
+import linkedInLink from '../../assets/Home/Social/linkedIn.png';
 import styles from './home.module.css';
 
 const home = props => {
     return (
         <div className={styles.Home}>
-            <img src={profilePic} alt="profilePic" />
+            <img src={profilePic} alt="profile picture" />
             <h1 className={styles.Greeting}>Hello, I'm Anthony.</h1>
             <p className={styles.Summary}>
                 I'm a Computer Science Graduate with a
                 concentraction in Software Engineering.
             </p>
-            <div>
+            <div className={styles.SocialLinks}>
+                <a href="https://github.com/Anthony9698">
+                    <img src={gitHubLink} alt="github link" />
+                </a>
+                <a href="https://www.linkedin.com/in/anthony-viera-4b58031aa/">
+                   <img src={linkedInLink} alt="linkedIn link" /> 
+                </a>
             </div>
         </div>
     );
