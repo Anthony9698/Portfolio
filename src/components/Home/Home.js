86fdc9d5f1b258a7dcd92b0f72385d560e4f2605
Anthony9698 <anthonyviera4@gmail.com>
2021-02-24 00:42:15 -0600

    Set up structure for landing page content.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './display.module.css';
+
+const home = props => {
+    return (
+        <div>
+            <img></img>
+            <div>Hello, I'm Anthony.</div>
+            <p>
+                I'm a Computer Science Graduate with a
+                concentraction in Software Engineering.
+            </p>
+            <div>
+                <img></img>
+                <img></img>
+            </div>
+        </div>
+    )
+}
