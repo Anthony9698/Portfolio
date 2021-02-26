c26c6a08779d8f0f375ab2fa43ed869dae770227
Anthony9698 <anthonyviera4@gmail.com>
2021-02-25 22:08:44 -0600

    Set up side nav drawer css.

--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import Home from '../../components/Home/Home';
 import About from '../../components/About/About';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 
 class Portfolio extends Component {
     render() {
         return (
-            <div>
+            <Aux>
                 <Toolbar />
                 <Home id="home" />
                 <About id="about" />
-            </div>
+            </Aux>
         );
     }
 }
