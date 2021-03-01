7d395901097d32d4bef0659629510452a3f837af
Anthony9698 <anthonyviera4@gmail.com>
2021-03-01 08:14:09 -0600

    Added title, desc, and button to about me page.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,18 @@ import React from 'react';
 import styles from './about.module.css';
 
 const about = props => (
-    <div className={styles.About} id={props.id}>About</div>
+    <div
+        className={styles.About}
+        id={props.id}>
+        <h2 className={styles.Title}>About Me</h2>
+        <p className={styles.Description}>
+            I love programming and learning new technologies
+            to solve complicated problems. I take pride in
+            developing elegant software solutions that are
+            easy to understand and efficient.
+        </p>
+        <button className={styles.DownloadResume}>Download Resume</button>
+    </div>
 );
 
 export default about;
