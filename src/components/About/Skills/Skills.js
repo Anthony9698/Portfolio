d5dd457a4e5f47311a83dc8fadd5e14c6eeb8a24
Anthony9698 <anthonyviera4@gmail.com>
2021-03-03 15:55:32 -0600

    Set up logic to fill skills bars dynamically.

--- src/components/About/Skills/Skills.js
+++ src/components/About/Skills/Skills.js
@@ -2,59 +2,16 @@ import React from 'react';
 import styles from './skills.module.css';
 
 const skills = props => {
-    let percentage = '65%';
+    let percentage = '90%';
     return (
         <div className={styles.Skills}>
             <h3>Skills</h3>
             <div className={styles.Graph}>
                 <div className={styles.GraphRow}>
                     <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
-                    <span>{percentage}</span>
-                </div>
-                <div className={styles.GraphRow}>
-                    <div className={styles.GraphRowLabel}>Java</div>
-                    <div className={styles.GraphRowProgress} style={{ width: `calc(${percentage} - 100px)` }}>.</div>
+                    <div className={styles.GraphRowProgress}>
+                        <div className={styles.Bar} style={{ '--width': percentage }}></div>
+                    </div>
                     <span>{percentage}</span>
                 </div>
             </div>
