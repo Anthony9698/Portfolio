3e4db822ed9606ba1c0e211767f0a935c40d5b2c
Anthony9698 <anthonyviera4@gmail.com>
2021-03-04 16:28:15 -0600

    Set up projects flexbox container.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,23 @@ import React from 'react';
 import styles from './projects.module.css';
 
 const projects = props => (
-    <div 
-        className={styles.Projects}
-        id={props.id}>
+    <div className={styles.Projects} id={props.id}>
+        <div className={styles.Title}>Projects</div>
+        <div className={styles.Content}>
+            <div className={styles.Description}>
+                <p>I try my best to stay busy whenever I can. Here are
+                some of the projects that I have worked on in the past.
+                </p>
+            </div>
+            <div className={styles.ProjectsDisplay}>
+                <div className={styles.Project}>1</div>
+                <div className={styles.Project}>1</div>
+                <div className={styles.Project}>1</div>
+                <div className={styles.Project}>1</div>
+                <div className={styles.Project}>1</div>
+                <div className={styles.Project}>1</div>
+            </div>
+        </div>
     </div>
 );
 
