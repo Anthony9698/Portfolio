616bfbc1d16c6c9e3ff8ce7515c413f90921613b
Anthony9698 <anthonyviera4@gmail.com>
2021-03-05 11:09:01 -0600

    Replaced projects media queries to scale instead with size of flex box.

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -6,7 +6,9 @@ import projectPlaceholder from '../../../assets/Projects/Placeholders/empty.jpg'
 const project = props => {
     return (
         <div className={styles.Project}>
-            <img className={styles.ProjectCoverImg} src={projectPlaceholder} alt="project cover" />
+            <div>
+                <img className src={projectPlaceholder} alt="project cover" />
+            </div>
             <div className={styles.ProjectTitle}>AmiTask</div>
         </div>
     );
