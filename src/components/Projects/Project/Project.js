9595c282b679ce0525345e48211514ea19fe7242
Anthony9698 <anthonyviera4@gmail.com>
2021-03-07 23:46:40 -0600

    Deleted unecessary comments in project component.

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -2,16 +2,13 @@ import React from 'react';
 import styles from './project.module.css';
 import projectPlaceholder from '../../../assets/Projects/Placeholders/empty.jpg';
 
-
 const project = props => {
     return (
         <div className={styles.Project}>
-            {/* <div className={styles.Overlay}></div> */}
             <div className={styles.Overlay}>
                 <img src={projectPlaceholder} alt="project cover" />
                 <div><button>Learn More</button></div>
             </div>
-            {/* <img className src={projectPlaceholder} alt="project cover" /> */}
             <div><a className={styles.ProjectTitle} href="#">AmiTask</a></div>
         </div>
     );
