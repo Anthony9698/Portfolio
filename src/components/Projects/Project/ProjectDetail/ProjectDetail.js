34bf0ebed02fa7a93f4c07f2ccc4d820c05ac00c
Anthony9698 <anthonyviera4@gmail.com>
2021-03-12 09:46:15 -0600

    Added View Site section.

--- src/components/Projects/Project/ProjectDetail/ProjectDetail.js
+++ src/components/Projects/Project/ProjectDetail/ProjectDetail.js
@@ -47,9 +47,23 @@ const projectDetail = props => {
                     </svg>
                 </div>
                 <div className={styles.Description}>
-                    <div className={styles.ProjectTitle}>Project Title</div>
-                    <div className={styles.ProjectSummary}>An Android Solution for Team Management</div>
-                    <div className={styles.BorderBottom}></div>
+                    <div className={styles.Title}>Project Title</div>
+                    <div className={styles.QuickSummary}>An Android Solution for Team Management</div>
+                    <div className={styles.Summary}>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type
+                            specimen book. It has survived not only five centuries, but also the leap into
+                            electronic typesetting, remaining essentially unchanged. It was popularised in
+                            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            and more recently with desktop publishing software like Aldus PageMaker
+                            including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.ViewMore}>
+                    <button>View Site</button>
                 </div>
             </div>
         </div >
