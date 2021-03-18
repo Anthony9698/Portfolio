112642cd64ddefc4c76576e27e161880a00d0bc4
Anthony9698 <anthonyviera4@gmail.com>
2021-03-18 14:40:41 -0500

    Fixed bug with offset in navigation links.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,10 +4,10 @@ import styles from './navigationItems.module.css';
 
 const navigationItems = props => (
     <ul className={styles.NavigationItems}>
-        <NavigationItem link="home" offset={-100}>Home</NavigationItem>
-        <NavigationItem link="about" offset={-50}>About</NavigationItem>
-        <NavigationItem link="projects" offset={-50}>Projects</NavigationItem>
-        <NavigationItem link="contact" offset={-50}>Contact</NavigationItem>        
+        <NavigationItem link="home">Home</NavigationItem>
+        <NavigationItem link="about">About</NavigationItem>
+        <NavigationItem link="projects">Projects</NavigationItem>
+        <NavigationItem link="contact">Contact</NavigationItem>        
     </ul>
 );
 
