3935f6998034dd44aa29165f2444e247b053f325
Anthony9698 <anthonyviera4@gmail.com>
2021-03-18 15:27:33 -0500

    Updated transition durations for buttons.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,20 +6,15 @@ import styles from './home.module.css';
 
 const home = props => (
     <div className={styles.Home} id={props.id}>
-        <div className={styles.Content}>
+        <div className={styles.MainContent}>
             <img className={styles.ProfilePic} src={profilePic} alt="profile" />
             <h1 className={styles.Greeting}>Hello, I'm Anthony.</h1>
             <p className={styles.Summary}>
                 I'm a Computer Science Graduate with a
                 concentration in Software Engineering.
             </p>
-            <div className={styles.SocialLinks}>
-                <a href="https://github.com/Anthony9698">
-                    <img src={gitHubLink} alt="github link" />
-                </a>
-                <a href="https://www.linkedin.com/in/anthony-viera-4b58031aa/">
-                    <img src={linkedInLink} alt="linkedIn link" />
-                </a>
+            <div className={styles.Start}>
+                <button>Start</button>
             </div>
         </div>
     </div>
