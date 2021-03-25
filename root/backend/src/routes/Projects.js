628a46508c338981eed7bde8bcf12801f7b198d6
Anthony9698 <anthonyviera4@gmail.com>
2021-03-25 18:47:02 -0500

    Created schema for sections.

--- root/backend/src/routes/Projects.js
+++ root/backend/src/routes/Projects.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Project = require('../models/Project');
+const Project = require('../models/Project/Project');
 
 // Get all projects
 router.get('/', async (req, res) => {
-    console.log("Hello");
     try {
         const projects = await Project.find();
         res.json(projects);
@@ -13,20 +12,23 @@ router.get('/', async (req, res) => {
     }
 });
 
-// Creating one
-router.post('/', async (req, res) => {
-    const project = new Project({
-        title: req.body.title,
-        summary: req.body.summary,
-        description: req.body.description,
-        tags: req.body.tags
-    });
-    try {
-        const newProject = await project.save();
-        res.status(201).json(newProject);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
+// Getting one
+router.get('/:id', getProject, (req, res) => {
+    res.send(res.project);
 });
+
+async function getProject(req, res, next) {
+    let project;
+    try {
+        project = await Project.findById(req.params.id);
+        if (project == null) {
+            return res.status(404).json({ message: 'Cannot find project' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+    res.project = project;
+    next();
+}
 
 module.exports = router;
