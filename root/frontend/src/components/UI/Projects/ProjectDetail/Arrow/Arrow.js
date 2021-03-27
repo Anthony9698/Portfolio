e506e356d5cb5cfff96918318d33bf608251d61f
Anthony9698 <anthonyviera4@gmail.com>
2021-03-27 01:12:36 -0500

    Moved carousel arrows into their own files.

---
+++ root/frontend/src/components/UI/Projects/ProjectDetail/Arrow/Arrow.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './arrow.module.css';
+
+const arrow = props => {
+    let attachedStyles = [styles.Arrow].join(' ');
+    if (props.direction === 'back') {
+        attachedStyles = [styles.Arrow, styles.Back].join(' ');
+    }
+    else if (props.direction === 'next') {
+        attachedStyles = [styles.Arrow, styles.Next].join(' ');
+    }
+    return (
+        <svg className={attachedStyles} xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36">
+            <path
+                id="Path_43"
+                fill="#E6E1E2"
+                data-name="Path 43"
+                d="M102.455,16.6,129.725.575a5.172,5.172,0,0,1,4.741,0l2.008,1.18a1.479,1.479,0,0,1,0,2.785l-22.9,13.453L136.5,31.46a1.48,1.48,0,0,1,0,2.785l-2.008,1.179a5.172,5.172,0,0,1-4.741,0L102.455,19.39a1.69,1.69,0,0,1-.977-1.4A1.689,1.689,0,0,1,102.455,16.6Z"
+                transform="translate(-101.478)" />
+        </svg>
+    );
+}
+export default arrow;
--- root/frontend/src/containers/Project/ProjectDetail/Carousel/Carousel.js
+++ root/frontend/src/containers/Project/ProjectDetail/Carousel/Carousel.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
 import styles from './carousel.module.css';
 import ImageSlide from '../../../../components/Projects/Project/ProjectDetail/Carousel/ImageSlide/ImageSlide';
+import Arrow from '../../../../components/UI/Projects/ProjectDetail/Arrow/Arrow';
 
 class Carousel extends Component {
     render() {
         return (
             <div className={styles.Carousel}>
                 <ImageSlide url={this.props.imageUrl} />
-                <svg className={styles.ImgArrowPrev} xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36">
-                    <path
-                        id="Path_43"
-                        fill="#E6E1E2"
-                        data-name="Path 43"
-                        d="M102.455,16.6,129.725.575a5.172,5.172,0,0,1,4.741,0l2.008,1.18a1.479,1.479,0,0,1,0,2.785l-22.9,13.453L136.5,31.46a1.48,1.48,0,0,1,0,2.785l-2.008,1.179a5.172,5.172,0,0,1-4.741,0L102.455,19.39a1.69,1.69,0,0,1-.977-1.4A1.689,1.689,0,0,1,102.455,16.6Z"
-                        transform="translate(-101.478)" />
-                </svg>
-                <svg className={styles.ImgArrowNext} id="right-arrow_1_" data-name="right-arrow (1)" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36">
+                <Arrow direction="back" />
+                <Arrow direction="next" />
+                {/* <svg className={styles.ImgArrowNext} id="right-arrow_1_" data-name="right-arrow (1)" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36">
                     <path
                         id="Path_43"
                         fill="#E6E1E2"
                         data-name="Path 43"
                         d="M136.5,16.6,109.231.575a5.172,5.172,0,0,0-4.741,0l-2.008,1.18a1.479,1.479,0,0,0,0,2.785l22.9,13.453L102.457,31.46a1.48,1.48,0,0,0,0,2.785l2.008,1.179a5.172,5.172,0,0,0,4.741,0L136.5,19.39a1.69,1.69,0,0,0,.977-1.4A1.689,1.689,0,0,0,136.5,16.6Z"
                         transform="translate(-101.478)" />
-                </svg>
+                </svg> */}
                 <svg className={styles.Exit} onClick={this.props.closeProjectDetail} xmlns="http://www.w3.org/2000/svg" width="26.995" height="24" viewBox="0 0 26.995 24">
                     <g id="Group_134" data-name="Group 134" transform="translate(-1754 -19292)">
                         <g id="Group_133" data-name="Group 133">
